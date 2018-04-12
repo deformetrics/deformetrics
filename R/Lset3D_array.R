46b8cca43f5a44e7ecc274bd65386b709103bdad
evoshape <deformetrics@gmail.com>
2018-04-12 12:11:15 +0100

    Add files via upload

---
+++ R/Lset3D_array.R
@@ -0,0 +1,8 @@
+#' @title example dataset
+#' @description Array containing a cranial 3D-landmark configuration acquired on a Primate sample
+#' @name Lset3D_array
+#' @docType data
+#' @author Paolo Piras, Antonio Profico, Valerio Varano, Pasquale Raia, Alessio Veneziano
+#' @keywords Arothron
+#' @usage data(Lset3D_array)
+NULL
---
+++ R/deformetrics.R
@@ -0,0 +1,10 @@
+#' @name deformetrics-package
+#' @docType package
+#' @aliases deformetrics
+#' @title  R Functions for Shape Analysis
+#' @author Paolo Piras, Antonio Profico, Valerio Varano, Pasquale Raia, Alessio Veneziano
+#' @description Tools for geometric morphometric analysis.
+#' @import Morpho
+#' @import stats
+#' @import abind
+NULL
